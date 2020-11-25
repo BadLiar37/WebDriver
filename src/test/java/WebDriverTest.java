dc7f949b6a06649787dcf1aad89d9018b0f1742b
BadLiar37 <valera.shumskij@gmail.com>
2020-11-26 00:02:10 +0300

    Six

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -20,8 +20,8 @@ public class WebDriverTest {
         menuElement.click();
         WebElement checkboxItem =new WebDriverWait(driver,30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@for, 'arrFilter_118_1717685296')]")));
-        String checkboxText=checkboxItem.getText().trim();
-        Assertions.assertEquals(checkboxText,"150 - 200 BYN");
+        String checkboxItemText=checkboxItem.getText().trim();
+        Assertions.assertEquals(checkboxItemText,"150 - 200 BYN");
         checkboxItem.click();
         WebElement popupButton=new WebDriverWait(driver, 30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div/div/div[3]/div[3]/div[2]/a")));
