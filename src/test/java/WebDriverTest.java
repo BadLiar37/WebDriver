9a3906c422a88ebcda4d678e9b7ae19ed91dfe5d
BadLiar37 <valera.shumskij@gmail.com>
2020-11-25 23:47:55 +0300

    Third

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -4,29 +4,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions;
 
-class WebDriverTest {
+public class WebDriverTest {
 
     @Test
-    void webdriverTest(){
+    public void webdriverTest(){
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://megatop.by/catalog/");
         driver.manage().window().maximize();
         WebElement searchField = new WebDriverWait(driver, 30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/header/div/div/div[1]/div/ul/li[2]")));
-        String strbutton=new WebDriverWait(driver, 30)
+        String buttonText=new WebDriverWait(driver, 30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/header/div/div/div[1]/div/ul/li[2]"))).getText().trim();
-        assertEquals(strbutton,"МУЖЧИНЫ");
+        Assertions.assertEquals(buttonText,"МУЖЧИНЫ");
         searchField.click();
         new WebDriverWait(driver,30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@for, 'arrFilter_118_1717685296')]"))).click();
-        String strcheckbox=new WebDriverWait(driver, 30)
+        String checkboxText=new WebDriverWait(driver, 30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@for, 'arrFilter_118_1717685296')]"))).getText().trim();
-        assertEquals(strcheckbox,"150 - 200 BYN");
-        String strpopupbutton=new WebDriverWait(driver, 30)
+        Assertions.assertEquals(checkboxText,"150 - 200 BYN");
+        String popupButtonText=new WebDriverWait(driver, 30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div/div/div[3]/div[3]/div[2]/a/span"))).getText().trim();
-        assertEquals(strpopupbutton,"ПОКАЗАТЬ");
+        Assertions.assertEquals(popupButtonText,"ПОКАЗАТЬ");
         new WebDriverWait(driver, 30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div/div/div[3]/div[3]/div[2]/a"))).click();
         driver.quit();
