c7101e1a37301efe97b1a8f544c227772f7c0651
BadLiar37 <valera.shumskij@gmail.com>
2020-11-25 23:58:37 +0300

    Fourth

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -1,10 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.junit.jupiter.api.Assertions;
 
 public class WebDriverTest {
 
@@ -13,22 +13,21 @@ public class WebDriverTest {
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://megatop.by/catalog/");
         driver.manage().window().maximize();
-        WebElement searchField = new WebDriverWait(driver, 30)
+        WebElement menuElement = new WebDriverWait(driver, 30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/header/div/div/div[1]/div/ul/li[2]")));
-        String buttonText=new WebDriverWait(driver, 30)
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/header/div/div/div[1]/div/ul/li[2]"))).getText().trim();
+        String buttonText=menuElement.getText().trim();
         Assertions.assertEquals(buttonText,"МУЖЧИНЫ");
-        searchField.click();
-        new WebDriverWait(driver,30)
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@for, 'arrFilter_118_1717685296')]"))).click();
-        String checkboxText=new WebDriverWait(driver, 30)
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@for, 'arrFilter_118_1717685296')]"))).getText().trim();
+        menuElement.click();
+        WebElement checkboxItem =new WebDriverWait(driver,30)
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@for, 'arrFilter_118_1717685296')]")));
+        String checkboxText=checkboxItem.getText().trim();
         Assertions.assertEquals(checkboxText,"150 - 200 BYN");
-        String popupButtonText=new WebDriverWait(driver, 30)
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div/div/div[3]/div[3]/div[2]/a/span"))).getText().trim();
+        checkboxItem.click();
+        WebElement popupButton=new WebDriverWait(driver, 30)
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div/div/div[3]/div[3]/div[2]/a")));
+        String popupButtonText=popupButton.getText().trim();
         Assertions.assertEquals(popupButtonText,"ПОКАЗАТЬ");
-        new WebDriverWait(driver, 30)
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div/div/div[3]/div[3]/div[2]/a"))).click();
+        popupButton.click();
         driver.quit();
     }
 }
