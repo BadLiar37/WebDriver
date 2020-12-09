18da93431bc786639b56205099981f6862c0b569
BadLiar37 <valera.shumskij@gmail.com>
2020-12-09 13:59:59 +0300

    Last changes

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -14,27 +14,27 @@ public class WebDriverTest {
     @BeforeEach
     public  void browserSetUp(){
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
     public void webdriverTest(){
         driver.get("https://megatop.by/catalog/");
-        driver.manage().window().maximize();
         WebElement menuElement = new WebDriverWait(driver, 30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/header/div/div/div[1]/div/ul/li[2]")));
         String menuElementText=menuElement.getText().trim();
-        Assertions.assertEquals(menuElementText,"МУЖЧИНЫ");
         menuElement.click();
         WebElement checkboxItem =new WebDriverWait(driver,30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@for, 'arrFilter_118_1717685296')]")));
         String checkboxItemText=checkboxItem.getText().trim();
-        Assertions.assertEquals(checkboxItemText,"150 - 200 BYN");
         checkboxItem.click();
         WebElement popupButton=new WebDriverWait(driver, 30)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div/div/div[3]/div[3]/div[2]/a")));
         String popupButtonText=popupButton.getText().trim();
-        Assertions.assertEquals(popupButtonText,"ПОКАЗАТЬ");
         popupButton.click();
+        Assertions.assertEquals(menuElementText,"МУЖЧИНЫ");
+        Assertions.assertEquals(checkboxItemText,"150 - 200 BYN");
+        Assertions.assertEquals(popupButtonText,"ПОКАЗАТЬ");
     }
 
     @AfterEach
